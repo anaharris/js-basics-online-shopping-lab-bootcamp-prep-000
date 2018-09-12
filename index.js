8b295c419b5a752de8b80125647ac6de99687f6d
Ana Harris <vana1990@gmail.com>
2018-09-12 17:45:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ function viewCart() {
   }
   else {
     for (var i=0; i<n-1; i++) {
-      message = message + `${cart[i].itemName} at $${cart[i].itemPrice}, `;
-      return message + `and ${getCart()[n].itemName} at $${cart[n].itemPrice}.`;
+      message = message + `${cartItem[i].itemName} at $${cart[i].itemPrice}, `;
+      return message + `and ${cart[n].itemName} at $${cart[n].itemPrice}.`;
     }
   }
 } 
