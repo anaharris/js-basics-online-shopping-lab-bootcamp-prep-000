65015a2e2a190f4fc79a8875409e08eabf41f80a
Ana Harris <vana1990@gmail.com>
2018-09-12 17:38:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,21 +10,38 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random()*100);
+  var cartItem ={itemPrice: price, itemName: item};
+  cart.push(cartItem);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
-}
+  var n = cart.length;
+  var message = 'In your cart, you have';
+  if (n<1) {
+    return "Your shopping cart is empty.";
+  }
+  else if (n===1) {
+    return `${message} ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+    
+  }
+  else {
+    for (var i=0; i<n-1; i++) {
+      message = message + `${cart[i].itemName} at $${cart[i].itemPrice}, `;
+      return message + `and ${cart[n].itemName} at $${cart[n].itemPrice}.`;
+    }
+  }
+} 
 
 function total() {
-  // write your code here
+  
 }
 
 function removeFromCart(item) {
-  // write your code here
+  
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  
 }
